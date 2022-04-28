868d0e384da663cc82edef88f80752377ca0be25
subhash2020 <subhash2016012759@gmail.com>
2022-04-28 22:14:03 +0545

    mahato003

--- list.dart
+++
@@ -1,5 +0,0 @@
-main() {
-  List<int> roll = [1, 2, 3, 4];
-  var s = roll[2];
-  print(roll);
-}
---
+++ subhash002.dart
@@ -0,0 +1,5 @@
+main() {
+  print("hello subhash");
+  print("you just created me to learn about github");
+  print("lalit");
+}
